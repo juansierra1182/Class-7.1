8a6194cdabe12d657bb5f85891c76a2fd08126b8
Mbeu199 <119823049+Mbeu199@users.noreply.github.com>
2023-02-02 20:23:43 -0600

    Add files via upload

---
+++ Questions .py
@@ -0,0 +1,15 @@
+1. Merge and clean Data 
+2. Create data frame with open, high, close, adj, and low.
+
+3. Analyze the Stocks that went up and down in the past few years with the lowest and highest values
+4. Five best and worst stocks invested in the past two years
+5. Do stocks stats, mean, median, and standard deviation.
+6. Create 5-line charts.
+
+7. what stock has the better & worst correlation to invest?  
+
+8. Betha Analysis  
+9. Correlation Matrix with all the stocks
+10. Written analysis comparing S&p500 with D.jo results.
+11. Add Summary  
+
